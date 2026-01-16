6264ee6207e3b20ea4ec0648911cb99e7caea299
cnobert <cnobert@nait.ca>
2026-01-16 13:42:14 -0700

    mid class

---
+++ Lesson03_Loops/Game1.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Lesson03_Loops;
+
+public class Lesson03Game : Game
+{
+    private GraphicsDeviceManager _graphics;
+    private SpriteBatch _spriteBatch;
+    private Texture2D _pixel;
+    private Vector2 _position, _dimensions;
+    private int _count;
+    private float _spacing;
+
+    public Lesson03Game()
+    {
+        _graphics = new GraphicsDeviceManager(this);
+        Content.RootDirectory = "Content";
+        IsMouseVisible = true;
+    }
+
+    protected override void Initialize()
+    {
+        _position = new Vector2(50, 200);
+        _dimensions = new Vector2(60, 40);
+
+        _count = 6; // number of rectangles
+        _spacing = 10; //10 pixels apart
+
+        base.Initialize();
+    }
+
+    protected override void LoadContent()
+    {
+        _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+        // TODO: use this.Content to load your game content here
+    }
+
+    protected override void Update(GameTime gameTime)
+    {
+        if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+            Exit();
+
+        // TODO: Add your update logic here
+
+        base.Update(gameTime);
+    }
+
+    protected override void Draw(GameTime gameTime)
+    {
+        GraphicsDevice.Clear(Color.CornflowerBlue);
+
+        // TODO: Add your drawing code here
+
+        base.Draw(gameTime);
+    }
+}
---
+++ Lesson03_Loops/Program.cs
@@ -0,0 +1,2 @@
+﻿using var game = new Lesson03_Loops.Lesson03Game();
+game.Run();
