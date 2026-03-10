b12742336f301127957727ef4870d7569040876d
cnobert <cnobert@nait.ca>
2026-03-10 10:54:03 -0600

    Mosquito

--- Lesson08MosquitoAttack/Cannon.cs
+++ Lesson08MosquitoAttack/Cannon.cs
@@ -7,12 +7,24 @@ namespace Lesson08MosquitoAttack;
 public class Cannon
 {
     private SimpleAnimation _animation;
-    private Vector2 _position;
+    private Vector2 _position, _direction;
     private Point _dimensions;
+    private float _speed;
 
-    internal void Initialize(Vector2 position)
+    internal Vector2 Direction 
+    { 
+        set
+        {
+            value.Y = 0;
+            _direction = value; 
+            _animation.Reverse = _direction.X < 0;
+        }
+    }
+
+    internal void Initialize(Vector2 position, float speed)
     {
         _position = position;
+        _speed = speed;
     }
 
     internal void LoadContent(ContentManager content)
@@ -24,7 +36,11 @@ public class Cannon
 
     internal void Update(GameTime gameTime)
     {
-        _animation.Update(gameTime);
+        float dt = (float) gameTime.ElapsedGameTime.TotalSeconds;
+        _position += _speed * _direction * dt;
+
+        if(_direction != Vector2.Zero)
+            _animation.Update(gameTime);
     }
     
     internal void Draw(SpriteBatch spriteBatch)
--- Lesson08MosquitoAttack/MosquitoAttackGame.cs
+++ Lesson08MosquitoAttack/MosquitoAttackGame.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.IO;
+﻿using System.IO;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
@@ -17,8 +16,20 @@ public class MosquitoAttackGame : Game
 
     private Texture2D _background;
     private SpriteFont _font;
+    private string _message = "";
 
     private Cannon _cannon;
+    private Mosquito _mosquito;
+
+    private KeyboardState _kbCurrentState, _kbPreviousState;
+
+    private enum GameState {Playing, Paused, Over}
+    private GameState _gameState = GameState.Playing;
+
+    private Rectangle BoundingBox
+    {
+        get { return new Rectangle(0, 0, _WindowWidth, _WindowHeight); }
+    }
 
     public MosquitoAttackGame()
     {
@@ -34,7 +45,10 @@ public class MosquitoAttackGame : Game
         _graphics.ApplyChanges();
 
         _cannon = new Cannon();
-        _cannon.Initialize(new Vector2(50, 325));
+        _cannon.Initialize(new Vector2(50, 325), 150);
+
+        _mosquito = new Mosquito();
+        _mosquito.Initialize(new Vector2( 100, 25), 150, new Vector2(-1, 0), BoundingBox);
 
         base.Initialize();
     }
@@ -45,7 +59,7 @@ public class MosquitoAttackGame : Game
         _background = Content.Load<Texture2D>("Background");
 
         _cannon.LoadContent(Content);
-        //_font = Content.Load<SpriteFont>("SystemArialFont");
+        _mosquito.LoadContent(Content);
 
         #region students, don't look here
         //MacOS ONLY
@@ -57,19 +71,69 @@ public class MosquitoAttackGame : Game
 
     protected override void Update(GameTime gameTime)
     {
-        _cannon.Update(gameTime);
+        _kbCurrentState = Keyboard.GetState();
+        switch(_gameState)
+        {
+            case GameState.Playing:
+                if(_kbCurrentState.IsKeyDown(Keys.A))
+                    _cannon.Direction = new Vector2(-1, 0);
+                else if(_kbCurrentState.IsKeyDown(Keys.D))
+                    _cannon.Direction  = new Vector2(1, 0);
+                else
+                    _cannon.Direction  = Vector2.Zero;
+                _cannon.Update(gameTime);
+                _mosquito.Update(gameTime);
+
+                if(Pressed(Keys.P))
+                {
+                    _gameState = GameState.Paused;
+                    _message = "Game Paused, press P to start playing.";
+                }
+
+                break;
+            case GameState.Paused:
+                if(Pressed(Keys.P))
+                {
+                    _gameState = GameState.Playing;
+                }
+                break;
+            case GameState.Over:
+                break;
+        }
+        _kbPreviousState = _kbCurrentState;
         base.Update(gameTime);
     }
 
     protected override void Draw(GameTime gameTime)
     {
         GraphicsDevice.Clear(Color.CornflowerBlue);
-
         _spriteBatch.Begin();
-        _spriteBatch.Draw(_background, Vector2.Zero, Color.White);
-        _cannon.Draw(_spriteBatch);
+
+        switch(_gameState)
+        {
+            case GameState.Playing:
+                _spriteBatch.Draw(_background, Vector2.Zero, Color.White);
+                _cannon.Draw(_spriteBatch);
+                _mosquito.Draw(_spriteBatch);
+                break;
+            case GameState.Paused:
+                _spriteBatch.Draw(_background, Vector2.Zero, Color.Silver);
+                _spriteBatch.DrawString(_font, _message, new Vector2(10, 135), Color.White);
+                _cannon.Draw(_spriteBatch);  
+                _mosquito.Draw(_spriteBatch);          
+                break;
+            case GameState.Over:
+                break;
+        }
+        
         _spriteBatch.End();
 
         base.Draw(gameTime);
+    }
+
+    private bool Pressed(Keys key)
+    {
+        // a new key press if it is down now, but was "up" 1/60 of a second ago
+        return _kbCurrentState.IsKeyDown(key) && _kbPreviousState.IsKeyUp(key);
     }
 }
